741b2521145649cc6d182e85f5e6e7ab33e3a86b
dhammastupa <35090785+dhammastupa@users.noreply.github.com>
2021-03-09 22:49:53 +0700

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBtf8GctGCOO94od6WI8m0kMiQK9VRzeqU',
-  authDomain: 'tptkcr.firebaseapp.com',
-  projectId: 'tptkcr',
-  storageBucket: 'tptkcr.appspot.com',
-  messagingSenderId: '246421686744',
-  appId: '1:246421686744:web:8b05b78f7207cb795dcaad',
-  measurementId: 'G-PKT6DLK2XQ'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
